fc5e434a8db25df081d09e0853a6a81d81cb3296
ISAIAH-max <mlewaisaiah@gmail.com>
2021-01-12 17:04:45 +0300

    A program that finds last digit of random assigned number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,8 @@ last_digit = int(repr(number)[-1])
 if number < 0:
     last_digit *= -1
 if last_digit > 5:
-    print("Last digit of {:d} is {:d} and is "
-      "greater than 5".format(number, last_digit))
+    print("Last digit of",number,"is",last_digit,"and is greater than 5")
 elif last_digit == 0:
-    print("Last digit of {:d} is {:d} and is "
-      "0".format(number, last_digit))
+    print("Last digit of",number,"is",last_digit,"and is 0")
 else:
-    print("Last digit of {:d} is {:d} and is "
-      "less than 6 and not 0".format(number, last_digit))
+    print("Last digit of",number,"is",last_digit,"and is less than 6 and not 0")
