cda1de0ab6b3d96b6938b14941ef39f66dd6c682
ISAIAH-max <mlewaisaiah@gmail.com>
2021-01-12 18:53:02 +0300

    A program that prints numbers from 00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for numbers in range(00, 100):
+for numbers in range(00, 99):
     print("{:02d}".format(numbers), end=", ")
-print("{:02d}".format(numbers))
+print("{:02d}".format(numbers + 1))
