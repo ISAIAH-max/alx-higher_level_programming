0ee091b6a432eebaec2091416379bbe555b61478
ISAIAH-max <mlewaisaiah@gmail.com>
2021-01-20 17:59:52 +0300

    Prints number and list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    x = len(sys.argv) - 1
+    if x == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments".format(x))
+        for y in range(x):
+            print("{}: {}".format(y+1, sys.argv[y+1]))
