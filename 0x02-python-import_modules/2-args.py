86ee1589e6570774bffc15ca58fd6b4686609f16
ISAIAH-max <mlewaisaiah@gmail.com>
2021-01-20 18:27:55 +0300

    Prints number and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,13 @@ import sys
 if __name__ == "__main__":
     x = len(sys.argv) - 1
     if x == 0:
-        print("{:d} {:s}.".format(x, "arguments"))
+        print("{:d} arguments.".format(x))
     else:
-        print("{:d} arguments:".format(x))
-        for y in range(x):
-            print("{:d}: {:s}".format(y+1, sys.argv[y+1]))
+        if x == 1:
+            print("{:d} argument:".format(x))
+            for y in range(x):
+                print("{:d}: {:s}".format(y+1, sys.argv[y+1]))
+        else:
+            print("{:d} arguments:".format(x))
+            for y in range(x):
+                print("{:d}: {:s}".format(y+1, sys.argv[y+1]))
