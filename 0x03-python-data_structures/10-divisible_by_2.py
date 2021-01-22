dc564b353d9f8dbea9401e15688c4959eaae368d
ISAIAH-max <mlewaisaiah@gmail.com>
2021-01-23 00:27:17 +0300

    Only Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     div = []
     for i in range(len(my_list)):
-        if my_list[i] %2 == 0:
+        if my_list[i] % 2 == 0:
             div.append(True)
         else:
             div.append(False)
