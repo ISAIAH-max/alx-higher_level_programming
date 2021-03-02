5d7e3170606fcdb5068653351905c902de227951
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-02 22:27:14 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,6 +4,5 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail.
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
