1efcb2d5b5f27d2562b14e4ecd965f745f37ec9f
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-23 00:44:04 +0300

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,11 +3,11 @@
 script that prints a square
 */
 'use strict';
-const x = process.argv[2];
+let x = process.argv[2];
 if (isNaN(x)) {
   console.log('Missing size');
 } else {
-  for (let n = 0; n < x; n++) {
-    console.log('x'.repeat(x));
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
   }
 }
