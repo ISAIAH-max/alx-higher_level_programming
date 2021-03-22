b8e81e20d7ca1e4a5d34556c2ad0195b28e8b104
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-22 23:59:38 +0300

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 'use strict';
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const idx in args) {
+let args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let idx in args) {
   console.log(args[idx]);
 }
