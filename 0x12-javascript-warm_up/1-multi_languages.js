e285ee08e43206092c0ecc3f637ee5f211a9663c
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-22 22:15:33 +0300

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -7,6 +7,4 @@ The third line: “Javascript is amazing”
 You must use console.log(...) to print all output
 You are not allowed to use var
 */
-console.log('C is fun');
-console.log('Python is cool');
-console.log('Javascript is amazing');
+console.log('C is fun\nPython is cool\nJavascript is amazing');
