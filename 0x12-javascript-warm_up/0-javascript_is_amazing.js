fd4f18bcd1df3a1e2591c1901483a8441085002e
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-22 18:45:40 +0300

    First constant, first print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -7,5 +7,5 @@ You must use console.log(...) to print all output
 You are not allowed to use var
 */
 
-const myVar = 'Javascript is amazing';
+const myVar = "Javascript is amazing";
 console.log(myVar);
