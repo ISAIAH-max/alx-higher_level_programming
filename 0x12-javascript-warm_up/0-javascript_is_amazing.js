6df0f557626597e969a7b8f9f556ff8ef39ada57
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-22 19:00:48 +0300

    First constant, first print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-/*
-Prints “Javascript is amazing”
-You must create a constant variable called myVar with
-the value “Javascript is amazing”
-You must use console.log(...) to print all output
-You are not allowed to use var
-*/
-
 const myVar = 'Javascript is amazing';
 console.log(myVar);
