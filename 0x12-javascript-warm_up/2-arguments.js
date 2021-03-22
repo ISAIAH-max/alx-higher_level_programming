06b4c64c207825f702ab84d170ad70caf1cabf9c
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-22 22:06:49 +0300

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*
+   A script that prints a message depending of the number of arguments passed:
+   If no arguments are passed to the script, print “No argument”
+   If only one argument is passed to the script, print “Argument found”
+   Otherwise, print “Arguments found”
+   You must use console.log(...) to print all output
+   You are not allowed to use var
+ */
+
+let j = process.argv.length;
+if (j === 2) {
+    console.log('No argument')
+}else if (j === 3) {
+    console.log('Argument found');
+}else {
+    console.log('Arguments found');
+}
