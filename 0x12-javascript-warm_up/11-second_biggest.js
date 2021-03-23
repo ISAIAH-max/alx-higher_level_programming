17ea7a331256b414b2168b8c3ef5bb707d7777c0
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-23 12:06:01 +0300

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 'use strict';
 let secBig = 0;
-const args = process.argv.slice(2);
+const args = process.argv.slice(2, process.argv.length);
 if (args.length > 1) {
-  args.sort();
+  args.sort((a, b) => a - b);
   secBig = args[args.length - 2];
 }
 console.log(secBig);
