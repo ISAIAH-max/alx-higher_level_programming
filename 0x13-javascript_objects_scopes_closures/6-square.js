7cc090a7ed5d5a3a5b2fdef7afd834be873ebd17
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-24 00:05:41 +0300

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    if ((c)) {
+    if (c) {
       for (let i = 0; i < this.height; i++) {
-        console.log('c'.repeat(this.width));
+        console.log('C'.repeat(this.width));
       }
     } else {
       this.print();
