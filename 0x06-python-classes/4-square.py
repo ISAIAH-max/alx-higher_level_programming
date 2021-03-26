30ba4f4fe7826a4e55f0228d21ee0720285d4881
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-26 19:58:23 +0300

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A class Square that defines a square by: (based on 3-square.py)
+"""
+
+
+class Square():
+     """
+     This is a class Square
+     """
+
+     def __init__(self, size):
+         if type(size) != int:
+             print("size must be an integer")
+             raise TypeError
+         elif (size < 0):
+             print("size must be >= 0")
+             raise ValueError
+         else:
+             self.__size = size
+
+     def area(self):
+         return (self.__size * self.__size)
+
+     @property
+     def size(self):
+         return (self.__size)
+
+     @size.setter
+     def size(self, value):
+         if type(value) != int:
+             print("size must be an integer", end="")
+             raise TypeError
+         elif (value < 0):
+             print("size must be >= 0", end="")
+             raise ValueError
+         else:
+             self.__size = value
