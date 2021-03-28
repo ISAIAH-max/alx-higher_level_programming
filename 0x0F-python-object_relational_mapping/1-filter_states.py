6e406201e8dc4637ab5f07328d9c9ef2f50c005f
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-29 00:05:47 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC WHERE BINARY "N%%"')
     rows = cur.fetchall()
     for row in rows:
         if row[1][0] == 'N':
