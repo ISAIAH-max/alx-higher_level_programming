cc561edc0fa71bf78884f84b9d137edefbe6134a
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-29 18:03:35 +0300

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+A class Square that defines a square by: (based on 3-square.py)
+"""
+
+
+class Square():
+    """
+    This is a class Square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            print("size must be an integer", end=" ")
+            raise TypeError
+        elif (size < 0):
+            print("size must be >= 0", end=" ")
+            raise ValueError
+        else:
+            self.__size = size
+        self.__position = position
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for x in range(self.__size):
+                print(" " * self.__position[0], end="")
+                for y in range(self.__size):
+                    print("#", end="")
+                print()
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if (value < 0):
+                print("size must be >= 0", end=" ")
+                raise ValueError
+            else:
+                self.__size = value
+        else:
+            print("size must be an integer", end="")
+            raise TypeError
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+       if type(value) is tuple and len(value) == 2 and type(value[0]) is int\
+          and type(value[1]) is int and value[0] >= 0 and value[1] >= 0:
+           self.__position = value
+       else:
+           raise TypeError("position must be a tuple of 2 positive integers")
