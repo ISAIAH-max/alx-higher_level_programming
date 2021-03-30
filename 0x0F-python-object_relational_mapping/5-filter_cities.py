9fbb31b320f768761a440278c2b19a3b92d9f2cb
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-30 22:52:06 +0300

    All cities by states

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd="Mlewaisaiah1998", db=argv[3], charset="utf8")
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     sql= "SELECT * FROM cities JOIN states ON\
     cities.state_id = states.id WHERE states.name=%s ORDER BY cities.id ASC"
