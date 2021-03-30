1acea17d9ae0173886497356ee2ca2b6ce688026
ISAIAH-max <mlewaisaiah@gmail.com>
2021-03-30 18:21:43 +0300

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,9 +11,9 @@ from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                           passwd="Mlewaisaiah1998", db=argv[3], charset="utf8")
     cur = conn.cursor()
-    sql = ("SELECT * FROM states WHERE name = '{:s}'ORDER BY id ASC")
+    sql = ("SELECT * FROM states WHERE name=%s ORDER BY id ASC")
     query_rows = cur.execute(sql, (argv[4]))
     for row in range(query_rows):
         print(cur.fetchone())
